7c9f593e50a63f2bf44be0ec6ad9e7df35a49b0d
adlomo23 <adlomo23@gmail.com>
2024-02-17 12:32:09 +0100

    Finished exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import Statistics from "./Statistics";
+import Button from "./Button";
+import StatisticLine from "./StatisticLine";
 
 const App = () => {
-  // save each button click in its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="Good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="Bad" />
+
+      {good + neutral + bad > 0 ? (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </div>
+  );
 };
 
 export default App;
